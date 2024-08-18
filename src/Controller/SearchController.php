88aa7ca5d127c204f7ad10ad858f80c7e85729c6
mohaoran69200 <mo.gounani@gmail.com>
2024-08-18 21:12:45 +0200

    petite modif

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -8,26 +8,36 @@ use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/search', name: 'app_')]
 class SearchController extends AbstractController
 {
-    #[Route('', name: 'search')]
-    public function index(Request $request, ProductRepository $productRepository): Response
-    {
-        $search = new SearchDto();
-        $form = $this->createForm(SearchType::class, $search);
-        $form->handleRequest($request);
+#[Route('', name: 'search')]
+public function index(Request $request, ProductRepository $productRepository): Response
+{
+$search = new SearchDto();
+$form = $this->createForm(SearchType::class, $search);
+$form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-             $results = $productRepository->search($search);
-        }
+// Initialisation des résultats
+$results = [];
 
+// Vérifie si le formulaire est soumis et valide
+if ($form->isSubmitted() && $form->isValid()) {
+$results = $productRepository->search($search);
+} else {
+// Vérifie si une recherche via la barre de recherche du header est envoyée
+$searchTerm = $request->query->get('search');
+if ($searchTerm) {
+$search->setSearch($searchTerm);
+$results = $productRepository->search($search);
+}
+}
 
-        return $this->render('search/index.html.twig', [
-            'form' => $form,
-            'results' => $results ?? []
-        ]);
-    }
+return $this->render('search/index.html.twig', [
+'form' => $form,
+'results' => $results
+]);
+}
 }
