2c26181ab1b94a09edbd82ba59a2b489712ce46f
Mohamed Gounani <mo.gounani@gmail.com>
2024-09-30 15:02:21 +0200

    Les crud sont fait

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -2,17 +2,27 @@
 
 namespace App\Controller\Admin;
 
+use App\Repository\ProductRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/admin', name: 'app_admin')]
 class AdminController extends AbstractController
 {
-    #[Route('/admin', name: 'app_admin')]
-    public function index(): Response
+    #[Route('/', name: 'admin_dashboard')]
+    public function dashboard(UserRepository $userRepository, ProductRepository $productRepository): Response
     {
+        //Je récupére le nombre total d'utilisateur
+        $totalUsers = $userRepository->count([]);
+
+        //Je récupére le nombre total d'annonces
+        $totalProducts = $productRepository->count([]);
+
         return $this->render('admin/index.html.twig', [
-            'controller_name' => 'AdminController',
+            'totalUsers' => $totalUsers,
+            'totalProducts' => $totalProducts,
         ]);
     }
 }
---
+++ src/Form/Product1Type.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Categorie;
+use App\Entity\Image;
+use App\Entity\Product;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class Product1Type extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('price')
+            ->add('unit')
+            ->add('stock')
+            ->add('createdAt', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('updatedAt', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('description')
+            ->add('categorie', EntityType::class, [
+                'class' => Categorie::class,
+                'choice_label' => 'id',
+            ])
+            ->add('image', EntityType::class, [
+                'class' => Image::class,
+                'choice_label' => 'id',
+            ])
+            ->add('user', EntityType::class, [
+                'class' => User::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Product::class,
+        ]);
+    }
+}
