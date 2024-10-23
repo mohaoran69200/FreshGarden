f886c3cdbde8a59e8258c688939fe2e5ea80703f
Mohamed Gounani <mo.gounani@gmail.com>
2024-10-23 17:17:47 +0200

    separation de styles a faire

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,19 @@
 import './bootstrap.js';
 
 import './styles/app.css';
+import './styles/base/_variable.css';
+import './styles/base/_form.css';
+import './styles/base/_button.css';
+import './styles/pages/_cart.css';
+import './styles/pages/_edit-profile.css';
+import './styles/pages/_home.css';
+import './styles/pages/_message.css';
+import './styles/components/_admin.css';
+import './styles/components/_card.css';
+import './styles/components/_footer.css';
+import './styles/components/_header.css';
+import './styles/components/_search.css';
+
 
 console.log('This log comes from assets/js/app.js - welcome to AssetMapper! 🎉');
 
