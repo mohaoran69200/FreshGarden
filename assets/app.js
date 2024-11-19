52daa52971eb16a4524884dd612211a670ef1902
Mohamed Gounani <mo.gounani@gmail.com>
2024-11-19 11:22:36 +0100

    pages d'informations faites

--- assets/app.js
+++ assets/app.js
@@ -13,6 +13,7 @@ import './styles/pages/_home.css';
 import './styles/pages/_message.css';
 import './styles/pages/_favorite.css';
 import './styles/pages/_order.css';
+import './styles/pages/_informations.css';
 import './styles/components/_admin.css';
 import './styles/components/_card.css';
 import './styles/components/_footer.css';
--- src/Controller/InformationController.php
+++ src/Controller/InformationController.php
@@ -2,7 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Message;
+use App\Entity\User;
+use App\Form\MessageType;
+use Doctrine\ORM\EntityManagerInterface;
+use LogicException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -10,10 +16,38 @@ use Symfony\Component\Routing\Attribute\Route;
 class InformationController extends AbstractController
 {
     #[Route('/contact', name: 'contact')]
-    public function contact(): Response
+    public function contact(Request $request, EntityManagerInterface $entityManager): Response
     {
-        return $this->render('information/contact.html.twig');
+        // Créez un nouveau message ou utilisez une entité existante
+        $message = new Message();
+        $form = $this->createForm(MessageType::class, $message);
+
+        // Gérez la soumission du formulaire
+        $form->handleRequest($request);
+
+        // Si le formulaire est soumis et valide, enregistrez-le ou effectuez une action
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $this->getUser();
+            if (!$user instanceof User) {
+                throw new LogicException('L\'utilisateur authentifié n\'est pas valide.');
+            }
+            $message->setSender($user);
+            $entityManager->persist($message);
+            $entityManager->flush();
+
+            // Ajoutez un message flash pour notifier l'utilisateur
+            $this->addFlash('message', 'Votre message a bien été envoyé.');
+
+            return $this->redirectToRoute('message_index');
+        }
+
+        // Passer le formulaire au template
+        return $this->render('information/contact.html.twig', [
+            'form' => $form->createView(),
+            'adminId' => 1,
+        ]);
     }
+
 
     #[Route('/conditions-generales-utilisation', name: 'cgu')]
     public function cgu(): Response
